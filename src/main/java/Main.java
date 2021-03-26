29c6641117fc5dce3702987e53ec23ad1280bf8c
Nicholas Zeng <taleorca@gmail.com>
2021-03-25 19:47:58 -0700

    finally works again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,32 @@
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main extends ListenerAdapter {
-    public static void main(String[] args) throws LoginException {
-        init();
+
+    private static final String BOT_TOKEN = Dotenv.load().get("BOT_TOKEN");
+
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        JDA api = JDABuilder.createDefault(BOT_TOKEN)
+                .addEventListeners(new Main())
+                .build()
+                .awaitReady();
     }
-
-    public static void init() throws LoginException {
-        String token = "";
-        Dotenv dotenv = Dotenv.load();
-        token = dotenv.get("BOT_TOKEN");
-
-        JDABuilder bot = new JDABuilder(AccountType.BOT);
-        bot.setToken(token);
-        bot.addEventListener(new Main());
-        bot.buildAsync();
-    }
-
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-
-        if (event.getAuthor().isBot()) {
-            return;
-        }
-
-        System.out.println(String.format("We received a message from %s: %s",
-                event.getAuthor().getName(),
-                event.getMessage().getContentDisplay()
-                ));
-        if (event.getMessage().getContentRaw().equals("!ping")) {
-            event.getChannel().sendMessage("Pong!").queue();
+        if (event.getAuthor().isBot()) return;
+        Message message = event.getMessage();
+        String content = message.getContentRaw();
+        if (content.equals("!ping")) {
+            MessageChannel channel = event.getChannel();
+            channel.sendMessage("Pong!").queue();
         }
     }
 }
+
